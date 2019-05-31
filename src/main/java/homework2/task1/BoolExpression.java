8c21be31f277f858fbaa4310354aaa85bce440d0
Igor Prykhodko <igorprihodkoemail@gmail.com>
2019-05-31 13:31:35 +0300

    Homework_2

--- src/main/java/homework2/task1/BoolExpression.java
+++ src/main/java/homework2/task1/BoolExpression.java
@@ -3,6 +3,6 @@ package homework2.task1;
 public class BoolExpression {
 
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a ^ b) && (c ^ d) || (a ^ c) && (b ^ d) || (a ^ d);
+        return (a ^ b) && (c ^ d) || (a ^ c) && (b ^ d);
     }
 }

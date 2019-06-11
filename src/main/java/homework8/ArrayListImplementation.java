a516343a238106e705101354ece5be8b13acb447
Igor Prykhodko <igorprihodkoemail@gmail.com>
2019-06-11 03:28:00 +0300

    Homework_8 Rebuild ArrayList

--- src/main/java/homework8/ArrayListImplementation.java
+++ src/main/java/homework8/ArrayListImplementation.java
@@ -1,77 +1,130 @@
 package homework8;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListImplementation<T> {
-
+    private static final Object[] EMPTY_ELEMENTDATA = {};
     private T[] typeArr;
     private int size;
 
-    public ArrayListImplementation() {
+    public ArrayListImplementation(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.typeArr = (T[]) new Object[initialCapacity];
+            this.size = 0;
+        } else if (initialCapacity == 0) {
+            this.typeArr =(T[]) EMPTY_ELEMENTDATA;
+            this.size = 0;
+        } else {
+            throw new IllegalArgumentException("Wrong capacity: " + initialCapacity);
+        }
     }
 
-    int size() {
-        int size = 0;
-        for (T aTypeArr : typeArr) {
-            size++;
-        }
+    private int size() {
         return size;
     }
 
-    boolean isEmpty() {
-        return size == 0;
+    private boolean isEmpty() {
+        return this.typeArr == EMPTY_ELEMENTDATA;
     }
 
-    void add(T value) {
-        T[] newArr = (T[]) new Object[size + 1];
+    private void add(T value) {
+        if (isEmpty() || size == typeArr.length) {
+            T[] newArr = (T[]) new Object[(typeArr.length * 3) / 2 + 1];
+            System.arraycopy(typeArr, 0, newArr, 0, typeArr.length);
+            typeArr = newArr;
+            typeArr[size] = value;
+        } else {
+            typeArr[size] = value;
+        }
+        size++;
+    }
+
+    private void add(T value, int index) {
+        if (index < 0) {
+            System.out.println("Wrong index. Need index >= 0");
+            return;
+        }
+        if (isEmpty()) {
+            typeArr = (T[]) new Object[index + 1];
+            typeArr[index] = value;
+        } else if (index <= typeArr.length) {
+            T[] newArr = (T[]) new Object[(typeArr.length * 3) / 2 + 1];
+            System.arraycopy(typeArr, 0, newArr, 0, index);
+            System.arraycopy(typeArr, index, newArr, index + 1, typeArr.length - index);
+            typeArr = newArr;
+            typeArr[index] = value;
+        } else if (index > typeArr.length) {
+            T[] newArr = (T[]) new Object[index + 1];
+            System.arraycopy(typeArr, 0, newArr, 0, typeArr.length);
+            typeArr = newArr;
+            typeArr[index] = value;
+        }
+        size++;
+    }
+
+    private void addAll(List<T> list) {
+        T[] newArr = (T[]) new Object[typeArr.length + list.size()];
         System.arraycopy(typeArr, 0, newArr, 0, typeArr.length);
+        System.arraycopy(list, 0, newArr, typeArr.length, list.size());
         typeArr = newArr;
-        typeArr[size++] = value;
-    }
-
-    void add(T value, int index) {
-        T[] newArr = (T[]) new Object[size + 1];
-        System.arraycopy(typeArr, 0 , newArr, 0, size - index);
-        System.arraycopy(typeArr, index, newArr, index + 1, size - index);
-        typeArr = newArr;
-        typeArr[index] = value;
-    }
-
-    void addAll(List<T> list) {
-        T[] newArr = (T[]) new Object[size + list.size()];
-        System.arraycopy(typeArr, 0, newArr, 0, typeArr.length);
-        System.arraycopy(list, 0, newArr, size + 1, list.size());
-        typeArr = newArr;
+        size = size + list.size();
     }
 
     T get(int index) {
         return typeArr[index];
     }
 
-    void set(T value, int index) {
-        typeArr[index] = value;
+    public void set(T value, int index) {
+        if (index >= 0) {
+            typeArr[index] = value;
+        } else {
+            System.out.println("Wrong index. Need index >= 0");
+        }
     }
 
-    T remove(int index) {
-        T value = typeArr[index];
-        T[] newArr = (T[]) new Object[size - 1];
-        System.arraycopy(typeArr, 0, newArr, 0, index);
-        System.arraycopy(typeArr, index + 1, newArr, index, size - index - 1);
-        typeArr = newArr;
-        return value;
+    public T remove(int index) {
+        if (index >= 0 && index < typeArr.length) {
+            T value = typeArr[index];
+            T[] newArr = (T[]) new Object[typeArr.length - 1];
+            System.arraycopy(typeArr, 0, newArr, 0, index);
+            System.arraycopy(typeArr, index + 1, newArr, index, typeArr.length - index - 1);
+            typeArr = newArr;
+            size--;
+            return value;
+        } else {
+            System.out.println("Wrong index. Need 0 <= index < size");
+            return null;
+        }
     }
 
-    T remove(T t) {
-        for (int i = 0; i < size; i++) {
-            T value = typeArr[i];
+    public T remove(T t) {
+        for (int i = 0; i < typeArr.length; i++) {
             if (typeArr[i] == t) {
-                T[] newArr = (T[]) new Object[size - 1];
-                System.arraycopy(typeArr, 0, newArr, 0, i);
-                System.arraycopy(typeArr, i + 1, newArr, i , size - i - 1);
-                typeArr = newArr;
-                return value;
+                remove(i);
             }
         }
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayListImplementation{" +
+                "typeArr=" + Arrays.toString(typeArr) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        ArrayListImplementation list = new ArrayListImplementation(10);
+        System.out.println(list.size());
+        list.add(100);
+        list.add(11);
+        list.add(2, 5);
+        System.out.println(list.toString());
+        list.add(2, -5);
+        System.out.println(list.toString());
+        list.remove(5);
+        System.out.println(list.toString());
     }
 }

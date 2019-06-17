1d1af15a01b20f5bcd92bb8e6b161fa73353d56c
Igor Prykhodko <igorprihodkoemail@gmail.com>
2019-06-17 23:56:59 +0300

    Homework_10. Fixed all marks, especial when key == null.

--- src/main/java/homework10/MyHashMap.java
+++ src/main/java/homework10/MyHashMap.java
@@ -2,7 +2,6 @@ package homework10;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 
 public class MyHashMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
@@ -11,6 +10,9 @@ public class MyHashMap<K, V> {
     private int size;
 
     public V get(K key) {
+        if (getNode(key)== null) {
+            return null;
+        }
         return getNode(key).getValue();
     }
 
@@ -25,83 +27,83 @@ public class MyHashMap<K, V> {
         if (positionNode == null) {
             arrayOfNodes[hash(key) % arrayOfNodes.length] = new Node<>(key, value, null, hash(key));
             size++;
-        } else if (positionNode.getKey().equals(key) && positionNode.hash == hash(key)) {
-            V oldValue = positionNode.getValue();
-            arrayOfNodes[hash(key) % arrayOfNodes.length].setValue(value);
-            return oldValue;
-        } else if (!(positionNode.getKey().equals(key)) && positionNode.hash == hash(key) ||
-                !(positionNode.getKey().equals(key)) && !(positionNode.hash == hash(key))) {
-            Node<K, V> nextInBucket = positionNode.next;
-            if (nextInBucket != null) {
-                do {
+        } else if ((key == null && positionNode.getKey() == null) ||
+                (!key.equals(positionNode.getKey()) && hash(key) == positionNode.hash) ||
+                !key.equals(positionNode.getKey()) && !(hash(key) == positionNode.hash)) {
+            if (positionNode.next == null) {
+                positionNode.next = new Node<>(key, value, null, hash(key));
+                size++;
+            } else {
+                Node<K, V> nextInBucket = positionNode.next;
+                while (nextInBucket.next != null) {
                     if (nextInBucket.key.equals(key)) {
                         V oldValue = nextInBucket.getValue();
                         nextInBucket.setValue(value);
                         return oldValue;
                     }
-                } while (nextInBucket.next != null);
-            } else {
-                nextInBucket = new Node<>(key, value, null, hash(key));
+                    nextInBucket = nextInBucket.next;
+                }
+                nextInBucket.next = new Node<>(key, value, null, hash(key));
                 size++;
             }
+        } else if (positionNode.getKey().equals(key)
+                && positionNode.hash == hash(key)) {
+            V oldValue = positionNode.getValue();
+            arrayOfNodes[hash(key) % arrayOfNodes.length].setValue(value);
+            return oldValue;
         }
         return null;
     }
 
     protected Node<K, V> removeNode(K key) {
-        if (getNode(key) != null) {
-            Node<K, V> currentNode = getNode(key);
-            if (currentNode.next != null) {
-                arrayOfNodes[hash(key) % arrayOfNodes.length] = currentNode.next;
-            } else {
-                arrayOfNodes[hash(key) % arrayOfNodes.length] = null;
-            }
-            size--;
-            return currentNode;
+        Node<K, V> currentNode = getNode(key);
+        if (currentNode.next != null) {
+            arrayOfNodes[hash(key) % arrayOfNodes.length] = currentNode.next;
+        } else {
+            arrayOfNodes[hash(key) % arrayOfNodes.length] = null;
         }
-        return null;
+        size--;
+        return currentNode;
     }
 
-    private Node<K, V> getNode(K key) {
-        Node<K, V> firstInBucket = arrayOfNodes[hash(key) % arrayOfNodes.length];
-        Node<K, V> nextInBucket = firstInBucket.next;
-        if (arrayOfNodes != null && arrayOfNodes.length > 0) {
-            if (firstInBucket != null) {
-                if (firstInBucket.key == null && key == null || firstInBucket.key.equals(key)) {
-                    return firstInBucket;
-                }
-                if (nextInBucket != null) {
-                    do {
-                        if (nextInBucket.key.equals(key)) {
-                            return nextInBucket;
-                        }
-                    } while (nextInBucket.next != null);
-                }
+    protected Node<K, V> getNode(K key) {
+        if (arrayOfNodes[hash(key) % arrayOfNodes.length] != null) {
+            Node<K, V> firstInBucket = arrayOfNodes[hash(key) % arrayOfNodes.length];
+            Node<K, V> nextInBucket = firstInBucket.next;
+            if (firstInBucket.key == null && key == null || firstInBucket.key.equals(key)) {
+                return firstInBucket;
+            }
+            if (nextInBucket != null) {
+                do {
+                    if (nextInBucket.key.equals(key)) {
+                        return nextInBucket;
+                    }
+                } while (nextInBucket.next != null);
             }
         }
         return null;
     }
 
-    private Node<K, V>[] resize(Node<K, V>[] oldArrayOfNodes) {
-        Node<K, V>[] newArrayOfNodes;
+    private Node[] resize(Node[] oldArrayOfNodes) {
+        Node[] newArrayOfNodes;
         if (oldArrayOfNodes.length > 0) {
             newArrayOfNodes = new Node[oldArrayOfNodes.length * 2];
         } else {
             newArrayOfNodes = new Node[DEFAULT_CAPACITY];
         }
-        Node<K, V> oldNode;
         for (int i = 0; i < oldArrayOfNodes.length; i++) {
+            Node oldNode;
             oldNode = oldArrayOfNodes[i];
             if (oldArrayOfNodes[i] != null) {
                 oldArrayOfNodes[i] = null;
                 if (oldNode.next == null) {
                     newArrayOfNodes[oldNode.hash % newArrayOfNodes.length] = oldNode;
                 } else {
-                    Node<K, V> oldNodeNext = oldNode.next;
                     do {
-                        newArrayOfNodes[oldNodeNext.hash % newArrayOfNodes.length] = oldNodeNext;
+                        newArrayOfNodes[oldNode.hash % newArrayOfNodes.length] = oldNode;
+                        oldNode = oldNode.next;
                     }
-                    while (oldNodeNext.next != null);
+                    while (oldNode.next != null);
                 }
             }
         }
@@ -143,24 +145,8 @@ public class MyHashMap<K, V> {
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node<?, ?> node = (Node<?, ?>) o;
-            return Objects.equals(getKey(), node.getKey()) &&
-                    Objects.equals(getValue(), node.getValue()) &&
-                    Objects.equals(next, node.next);
-        }
-
-        @Override
-        public int hashCode() {
-
-            return Objects.hash(getKey(), getValue(), next);
-        }
-
-        @Override
         public String toString() {
-            return '\n' + "Node{" +
+            return "Node{" +
                     "key=" + key +
                     ", value=" + value +
                     ", next=" + next +
@@ -170,14 +156,14 @@ public class MyHashMap<K, V> {
 
     private static int hash(Object key) {
         int hashKey;
-        return (key == null) ? 0 : (hashKey = key.hashCode())/* ^ (hashKey >>> 16)*/;
+        return (key == null) ? 0 : (hashKey = key.hashCode()) ^ (hashKey >>> 16);
     }
 
     @Override
     public String toString() {
-        return "MyHashMap{" +
+        return "MyHashMap " +
                 "arrayOfNodes=" + Arrays.toString(arrayOfNodes) +
                 ", size=" + size +
-                '}' + ", capacity=" + arrayOfNodes.length + '\n';
+                ", capacity=" + arrayOfNodes.length + '\n';
     }
 }

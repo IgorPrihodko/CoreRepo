82ae7a6adcecc60b3727f628b95572da217a66e6
Igor Prykhodko <igorprihodkoemail@gmail.com>
2019-06-25 02:45:45 +0300

    Homework_13. Fixed Deserializable. Rebuilt MinMax.

--- src/main/java/homework13/task1/MinMax.java
+++ src/main/java/homework13/task1/MinMax.java
@@ -1,20 +1,22 @@
 package homework13.task1;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MinMax {
     public static <T> void findMinMax(Stream<? extends T> stream, Comparator<? super T> order,
                                       BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        T[] arr = (T[]) stream.sorted(order).toArray();
-        if (arr.length > 0) {
-            minMaxConsumer.accept(arr[0], arr[arr.length - 1]);
-        } else {
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (list.size() == 0) {
             minMaxConsumer.accept(null, null);
+        } else {
+            T max = list.stream().max(order).get();
+            T min = list.stream().min(order).get();
+            minMaxConsumer.accept(min, max);
         }
-        /*List<? extends T> list = stream.collect(Collectors.toList());
-        minMaxConsumer.accept(Collections.min(list, order), Collections.max(list, order));*/
     }
 }
--- src/main/java/homework13/task2/Deserializable.java
+++ src/main/java/homework13/task2/Deserializable.java
@@ -6,20 +6,18 @@ import java.io.ObjectInputStream;
 
 public class Deserializable {
     public static Animal[] deserializeAnimalArray(byte[] data) {
-
-        Animal[] animals = null;
         try(ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data))) {
             int size = objectInputStream.readInt();
-            animals = new Animal[size];
+            Animal[] animals = new Animal[size];
             for (int i = 0; i < size; i++) {
                 animals[i] = (Animal) objectInputStream.readObject();
             }
             for (Animal a : animals) {
                 System.out.println(a);
             }
+            return animals;
         } catch (IOException | ClassNotFoundException | ClassCastException e) {
             throw new IllegalArgumentException();
         }
-        return animals;
     }
 }
